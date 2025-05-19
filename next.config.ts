25b45bc611b03fe65677248e53c47f36ba793615
Mbedobe K <danielbushbaby@gmail.com>
2025-05-19 20:19:12 +0000

    update image settings to show

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   output: "export",
+  images: {
+    unoptimized: true, // 👈 Required for static export
+  },
 };
 
 export default nextConfig;
